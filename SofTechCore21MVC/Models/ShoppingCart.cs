60c09021d93f1429c47f2f3db952cdbaf16d2aa6
DerekPeacock <derek.peacock@yahoo.com>
2019-11-20 20:27:45 +0000

    Updated the ShoopingCart

--- SofTechCore21MVC/Models/ShoppingCart.cs
+++ SofTechCore21MVC/Models/ShoppingCart.cs
@@ -1,60 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 namespace SofTechCore21MVC.Models
 {
     public class ShoppingCart
     {
-        private int shoppingCartID;
-        public int ShoppingCartID
-        {
-            get
-            {
-                return shoppingCartID;
-            }
-            set
-            {
-                shoppingCartID = value;
-            }
-        }
-        private int customerID;
-        public int CustomerID
-        {
-            get
-            {
-                return customerID;
-            }
-            set
-            {
-                customerID = value;
-            }
-        }
-        private int orderDate;
-        public int OrderDate
-        {
-            get
-            {
-                return orderDate;
-            }
-            set
-            {
-                orderDate = value;
-            }
-        }
-        private string status;
-        public string Status
-        {
-            get
-            {
-                return status;
-            }
-            set
-            {
-                status = value;
-            }
-        }
+        public int ShoppingCartID { get; set; }
 
-        private OrderItem[] part_of;
-        private Customer[] may_add_Garments_to;
+        public int CustomerID { get; set; }
 
+        public int OrderDate { get; set; }
+
+        public string Status { get; set; }
+
+        // navigation properties (relationships)
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        public virtual Customer Customer { get; set; }
     }
+
 }
