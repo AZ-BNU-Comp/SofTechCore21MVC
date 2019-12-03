00977e09da445f01d66931bb001101c00f68c2ed
Abdi Esse <21211756@bucks.ac.uk>
2019-12-03 15:45:41 +0000

    edit1

--- SofTechCore21MVC/Models/Address.cs
+++ SofTechCore21MVC/Models/Address.cs
@@ -7,15 +7,31 @@ namespace SofTechCore21MVC.Models
     /// </summary>
     public class Address
     {
+        [Key, StringLength(50)]
         public int AddressID { get; set; }
+        [StringLength(50)]
         public int CustomerID { get; set; }
+        [Required, StringLength(50), DisplayName("Customer ID Name")]
         public int HouseNumber { get; set; }
+        [Required, StringLength(50), DisplayName("HouseNumber")]
         public string StreetName { get; set; }
+        [Required, DisplayName("Street Name")]
         public string County { get; set; }
+        [Requried, DisplayName("County")]
         public string City { get; set; }
+        [Required, DisplayName("City")]
         public string Country { get; set; }
+        [Required, DisplayName("Country")]
         public string PostCode { get; set; }
         private Customer Customer { get; set; }
+        public virtual ICollection<Gender> Gender {get; set;}
+        public virtual ICollection<Customer>Customer {get; set;}
+        public virtual ICollection<PaymentCard>PaymentCard {get; set;}
+        public virtual ICollection<ShoppingCart>ShoppingCart {get; set;}
+        public virtual ICollection<OrderItem>OrderItem {get; set;}
+        public virtual ICollection<Review>Review{get; set;}
+        public virtual ICollection<Favourite>Favourite{get; set;}
+        public virtual ICollection<Complaint>Complaint{get; set;}
        
         
         
--- SofTechCore21MVC/Models/OrderItem.cs
+++ SofTechCore21MVC/Models/OrderItem.cs
@@ -8,12 +8,25 @@ namespace SofTechCore21MVC.Models
     public class OrderItem
     {
        
+        [Key, StringLength(50)]
         public int OrderItemID { get; set; }
+        [Required, DisplayName("OrderItem ID")]
         public int GarmentID { get; set; }
+        [Required, DisplayName("Garment ID")]
         public int ShoppingCartID { get; set; }
+        [Required, StringLength(50), DisplayName("ShoppingCart ID")]
         private ShoppingCart[] contains;
         private Garment[] may_be_an;
         private Complaint[] is_featured_on;
+        public virtual ICollection<Gender> Gender { get; set; }
+        public virtual ICollection<Address> Address { get; set; }
+        public virtual ICollection<PaymentCard> PaymentCard { get; set; }
+        public virtual ICollection<Customer> Customer { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<Favourite> Favourites { get; set; }
+        public virtual ICollection<Complaint> Complaints { get; set; }
+        /// Created a billing address model ///
+        
 
         
        
