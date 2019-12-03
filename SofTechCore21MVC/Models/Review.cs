4d9f774f382cd4b9a7e7b4423eb04186907a1130
DerekPeacock <derek.peacock@yahoo.com>
2019-12-03 19:33:47 +0000

    Derek: Review corrected

--- SofTechCore21MVC/Models/Review.cs
+++ SofTechCore21MVC/Models/Review.cs
@@ -12,19 +12,31 @@ namespace SofTechCore21MVC.Models
     {
         [Key]
         public int ReviewID { get; set; }
+        
         [Required]
         public int CustomerID { get; set; }
+        
         [Required]
         public int GarmentID { get; set; }
+        
         [Required, StringLength(50)]
         public string Title { get; set; }
+        
         [Required, StringLength(4000, ErrorMessage = "There is a 4000 character limit.")]
         public string Statement { get; set; }
+        
+        [Range(1.0, 5.0)]
         public float Rating { get; set; }
+        
         [Required, Display(Name = "Date of Review"), DataType(DataType.Date),
             DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public int ReviewDate { get; set; }
-        public virtual ICollection<OrderItem> OrderItem { get; set; }
+
+        // Navigation Properties
+
+        //public virtual ICollection<OrderItem> OrderItem { get; set; }
         public virtual Customer Customer { get; set; }
+
+        public virtual Garment Garment { get; set; }
     }
 }
