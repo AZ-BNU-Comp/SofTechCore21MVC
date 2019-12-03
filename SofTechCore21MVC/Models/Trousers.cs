dbbf3b41b53dd2272df3d98e9eccec446515da64
DerekPeacock <derek.peacock@yahoo.com>
2019-12-03 20:15:17 +0000

    updated message about GitHub Wiki

--- SofTechCore21MVC/Models/Trousers.cs
+++ SofTechCore21MVC/Models/Trousers.cs
@@ -4,7 +4,7 @@ namespace SofTechCore21MVC.Models
 {
     /// <summary>
     /// Rohail Siddiqui
-    /// You may not want this class.  Please read 
+    /// You may not want this class.  Please read the Wiki in GitHub
     /// You will get extra marks for inheritance if you add it to your evaluation
     /// but I think it is better in this case to just have a single Garment class
     /// </summary>
