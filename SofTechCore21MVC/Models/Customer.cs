4a9283758631ba26f619ad6a3a3bf127b4e7c66c
DerekPeacock <derek.peacock@yahoo.com>
2019-12-03 19:18:51 +0000

    Derek: Corrected Customer

--- SofTechCore21MVC/Models/Customer.cs
+++ SofTechCore21MVC/Models/Customer.cs
@@ -10,27 +10,45 @@ namespace SofTechCore21MVC.Models
     /// </summary>
     public class Customer
     {
-        [Key, StringLength(50)]
+        [Key]
         public int CustomerID { get; set; }
+        
         [StringLength(50)]
         public string UserID { get; set; }
-        [Required, StringLength(50), DisplayName("First Name")]
+        
+        [Required, StringLength(20), DisplayName("First Name")]
         public string FirstName { get; set; }
-        [Required, StringLength(50), DisplayName("Last Name")]
+
+        [Required, StringLength(20), DisplayName("Last Name")]
         public string Surname { get; set; }
+
         [Required, Display(Name ="Date of Birth"), DataType(DataType.Date), 
             DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
-        [Required, DataType(DataType.EmailAddress)]
+
+        public Gender Gender { get; set; }
+
+        [Required, StringLength(100), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required, StringLength(11, ErrorMessage = "Please enter a valid UK contact number")]
+
+        [Required, StringLength(16, ErrorMessage = "Please enter a valid UK contact number")]
         public PhoneNumber PhoneNumber { get; set; }
-        public virtual ICollection<Gender> Gender { get; set; }
-        public virtual ICollection<Address> Address { get; set; }
-        public virtual ICollection<PaymentCard> PaymentCard { get; set; }
+
+        // Navigation Properties
+        //public virtual ICollection<Gender> Gender { get; set; }
+
+        // For simplicity use a single address
+        public virtual Address Address { get; set; }
+
+        // For simplicity use a single PaymentCard
+        public virtual PaymentCard PaymentCard { get; set; }
+
         public virtual ICollection<ShoppingCart> Orders { get; set; }
+
         public virtual ICollection<Review> Reviews { get; set; }
+
         public virtual ICollection<Favourite> Favourites { get; set; }
+
         public virtual ICollection<Complaint> Complaints { get; set; }
         
         // NOT NEEDED
