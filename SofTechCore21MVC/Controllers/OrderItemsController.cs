617a2f691d16bab380df17e9f068704b3b351e53
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2019-12-05 10:41:34 +0000

    Added ShoppingCart and OrderItem CRUD pages

---
+++ SofTechCore21MVC/Controllers/OrderItemsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SofTechCore21MVC.Data;
+using SofTechCore21MVC.Models;
+
+namespace SofTechCore21MVC.Controllers
+{
+    public class OrderItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderItems
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Orderitem.Include(o => o.Garment).Include(o => o.ShoppingCart);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: OrderItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.Orderitem
+                .Include(o => o.Garment)
+                .Include(o => o.ShoppingCart)
+                .FirstOrDefaultAsync(m => m.OrderItemID == id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Create
+        public IActionResult Create()
+        {
+            ViewData["GarmentID"] = new SelectList(_context.Garment, "GarmentID", "GarmentID");
+            ViewData["ShoppingCartID"] = new SelectList(_context.ShoppingCart, "ShoppingCartID", "ShoppingCartID");
+            return View();
+        }
+
+        // POST: OrderItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderItemID,GarmentID,ShoppingCartID,Quantity,PurchasePrice")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GarmentID"] = new SelectList(_context.Garment, "GarmentID", "GarmentID", orderItem.GarmentID);
+            ViewData["ShoppingCartID"] = new SelectList(_context.ShoppingCart, "ShoppingCartID", "ShoppingCartID", orderItem.ShoppingCartID);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.Orderitem.FindAsync(id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["GarmentID"] = new SelectList(_context.Garment, "GarmentID", "GarmentID", orderItem.GarmentID);
+            ViewData["ShoppingCartID"] = new SelectList(_context.ShoppingCart, "ShoppingCartID", "ShoppingCartID", orderItem.ShoppingCartID);
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderItemID,GarmentID,ShoppingCartID,Quantity,PurchasePrice")] OrderItem orderItem)
+        {
+            if (id != orderItem.OrderItemID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderItemExists(orderItem.OrderItemID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GarmentID"] = new SelectList(_context.Garment, "GarmentID", "GarmentID", orderItem.GarmentID);
+            ViewData["ShoppingCartID"] = new SelectList(_context.ShoppingCart, "ShoppingCartID", "ShoppingCartID", orderItem.ShoppingCartID);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderItem = await _context.Orderitem
+                .Include(o => o.Garment)
+                .Include(o => o.ShoppingCart)
+                .FirstOrDefaultAsync(m => m.OrderItemID == id);
+            if (orderItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var orderItem = await _context.Orderitem.FindAsync(id);
+            _context.Orderitem.Remove(orderItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderItemExists(int id)
+        {
+            return _context.Orderitem.Any(e => e.OrderItemID == id);
+        }
+    }
+}
