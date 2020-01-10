0c13ad1e88467509e210898f37ca0186982c5e81
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-01-10 14:10:44 +0000

    Edited Customers, Home and PaymentsCards Controllers | Views:Home | Layouts + _CustomerDetails

--- SofTechCore21MVC/Controllers/HomeController.cs
+++ SofTechCore21MVC/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SofTechCore21MVC.Data;
 using SofTechCore21MVC.Models;
 
 namespace SofTechCore21MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -38,6 +45,38 @@ namespace SofTechCore21MVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult MyAccount()
+        {
+            string user = User.Identity.Name;
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var customer = _context.Customer
+                .FirstOrDefault(m => m.Email == user);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+        public IActionResult Women()
+        {
+            return View();
+        }
+        public IActionResult Men()
+        {
+            return View();
+        }
+        public IActionResult Accessories()
+        {
+            return View();
         }
     }
 }
